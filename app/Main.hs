1b97de54c3f847984c0b7dba8c6d3c077d19209c
Kevin Mullins <kevin@pnotequalnp.com>
2021-08-09 12:11:23 -0700

    Revamp CLI and with standalone mode

--- app/Main.hs
+++ app/Main.hs
@@ -1,47 +1,80 @@
 module Main where
 
-import Control.Category ((>>>))
-import Data.Char (toLower)
 import Data.Foldable (fold)
 import Data.Version (showVersion)
 import Editor.Client qualified as Client
+import Editor.Client.Network.Channel qualified as Client.Channel
 import Editor.Client.Network.Unix qualified as Client.Unix
 import Editor.Network (getDefaultPath)
 import Editor.Server qualified as Server
+import Editor.Server.Network.Channel qualified as Server.Channel
 import Editor.Server.Network.Unix qualified as Server.Unix
 import Options.Applicative
 import Paths_editor (version)
+import UnliftIO.Async (async)
+import UnliftIO.Chan (newChan)
 
-data Mode = Client | Server
+data Mode = Client SocketInfo | Server SocketInfo | Standalone
 
-data Network = Unix | TCP
+data SocketType = Unix | TCP
 
-data Opts = Opts
-  { mode :: Mode,
-    path :: Maybe String,
-    network :: Maybe Network
+data SocketInfo = SocketInfo
+  { socketType :: SocketType,
+    socketPath :: Maybe FilePath
   }
 
 main :: IO ()
-main = execParser (info parser cliOpts) >>= execute
+main = execParser (info parser cliOpts) >>= run
   where
-    cliOpts =
-      fold
-        [ progDesc "Basic text editor. Almost certainly not useful."
-        ]
+    cliOpts = progDesc "Basic text editor. Almost certainly not useful."
 
-execute :: Opts -> IO ()
-execute Opts {mode, path} = do
-  addr <- maybe Editor.Network.getDefaultPath pure path
-  case mode of
-    Client -> Client.Unix.init addr >>= Client.main
-    Server -> Server.Unix.init addr >>= Server.main
+run :: Mode -> IO ()
+run Standalone = do
+  toServer <- newChan
+  toClient <- newChan
+  env <- Server.initEnv
+  _serverThread <- async . Server.server env $ Server.Channel.Channel toClient toServer
+  Client.main $ Client.Channel.Channel toClient toServer
+run (Client SocketInfo {socketPath}) = do
+  path <- maybe getDefaultPath pure socketPath
+  Client.Unix.init path >>= Client.main
+run (Server SocketInfo {socketPath}) = do
+  path <- maybe getDefaultPath pure socketPath
+  Server.Unix.init path >>= Server.main
 
-parser :: Parser Opts
-parser = opts <**> helper <**> versioner
+parser :: Parser Mode
+parser = parseMode <**> helper <**> versioner
 
-opts :: Parser Opts
-opts = Opts <$> parseMode <*> parsePath <*> parseNetwork
+parseMode :: Parser Mode
+parseMode =
+  (serverFlag <|> clientFlag)
+    <*> parseSocketInfo
+    <|> pure Standalone
+
+serverFlag :: Parser (SocketInfo -> Mode)
+serverFlag = flag' Server (fold [short 'd', long "daemon", help "Run server daemon"])
+
+clientFlag :: Parser (SocketInfo -> Mode)
+clientFlag = flag' Client (fold [short 'c', long "client", help "Run client"])
+
+parseSocketInfo :: Parser SocketInfo
+parseSocketInfo = SocketInfo <$> parseSocketType <*> parsePath
+
+parseSocketType :: Parser SocketType
+parseSocketType = unixFlag <|> pure Unix
+
+unixFlag :: Parser SocketType
+unixFlag = flag' Unix $ fold [long "unix", help "Use Unix domain socket (default)"]
+
+parsePath :: Parser (Maybe FilePath)
+parsePath =
+  optional . strOption $
+    fold
+      [ short 'p',
+        long "path",
+        help "Socket path",
+        metavar "PATH"
+      ]
 
 versioner :: Parser (a -> a)
 versioner =
@@ -51,39 +84,4 @@ versioner =
         long "version",
         help "Show version",
         hidden
-      ]
-
-parseMode :: Parser Mode
-parseMode =
-  flag Client Server $
-    fold
-      [ short 'd',
-        long "daemon",
-        help "Run server daemon"
-      ]
-
-parseNetwork :: Parser (Maybe Network)
-parseNetwork =
-  optional . option (maybeReader p) $
-    fold
-      [ short 'b',
-        long "backend",
-        help "Socket type (currently ignored)",
-        metavar "UNIX|TCP"
-      ]
-  where
-    p =
-      fmap toLower >>> \case
-        "unix" -> Just Unix
-        "tcp" -> Just TCP
-        _ -> Nothing
-
-parsePath :: Parser (Maybe String)
-parsePath =
-  optional . strOption $
-    fold
-      [ short 'p',
-        long "path",
-        help "Socket path",
-        metavar "PATH"
       ]
